a1bb200e2c6e56b33baff2479f09221fe65bb023
bd65 <bd65@students.waikato.ac.nz>
2025-11-24 09:07:40 +1300

    styling changes

--- app/done/page.tsx
+++ app/done/page.tsx
@@ -2,7 +2,9 @@ import { Button } from "../../components/ui/button";
 
 export default function Page() {
     return <div className="pt-40 text-center">
-        <p>All done!</p>
+        <p>All done! Shots for your shot 📸 </p>
+
+        <p>Watch to see your photo in the slideshow once it&lsquo;s approved! </p>
         
         <p><Button><a href="/upload">Add another</a></Button></p>
         </div>;
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,15 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <h1>EventCam</h1>
+        <div></div>
+        <h1 className="text-4xl">EventCam 📸</h1>
+        
+        <p>Get your photo on the big screen!</p>
 
         <Link href="/upload" className="underline"> Upload Event Photo</Link>
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <Link href="/protected/image-review">Protected Image Review Page</Link>
+          <Link href="/protected/image-review">Image Review</Link>
+          <Link href="/protected/slideshow">Slideshow</Link>
           <ThemeSwitcher />
         </footer>
       </div>
