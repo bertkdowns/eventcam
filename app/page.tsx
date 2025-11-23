be8e335df137b8777776743a370890ca25e0ef6f
bd65 <bd65@students.waikato.ac.nz>
2025-11-24 09:11:29 +1300

    more styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,11 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <div></div>
-        <h1 className="text-4xl">EventCam 📸</h1>
+        <h1 className="text-4xl w-full border-b py-16 text-center">EventCam 📸</h1>
         
         <p>Get your photo on the big screen!</p>
 
-        <Link href="/upload" className="underline"> Upload Event Photo</Link>
+        <Link href="/upload" className="underline text-xl"> Upload Event Photo</Link>
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <Link href="/protected/image-review">Image Review</Link>
           <Link href="/protected/slideshow">Slideshow</Link>
