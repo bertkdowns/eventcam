8b41f7a42070162ed89e439836b5e776d8eddc07
Bert <bertkdowns@gmail.com>
2025-12-02 18:32:10 +1300

    fix spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import ImageUploader from "./upload/image-uploader";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <h1 className="text-4xl w-full border-b py-16 text-center">EventCam 📸</h1>
+      <div className="flex-1 w-full flex flex-col gap-4 items-center">
+        <h1 className="text-4xl w-full border-b py-4 text-center">EventCam 📸</h1>
         
         <p>Get your photo on the big screen!</p>
 
         <ImageUploader />
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-8">
           <Link href="/protected/image-review">Image Review</Link>
           <Link href="/protected/slideshow">Slideshow</Link>
           <ThemeSwitcher />
